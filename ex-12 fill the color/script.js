3f2745e43fda2c3be4b3e71de2a1cf8bdc4151d3
Saurabhpriyadarshi0810 <spriyadarshi831@gmail.com>
2025-06-19 17:18:02 +0530

    new channges

---
+++ ex-12 fill the color/script.js
@@ -0,0 +1,68 @@
+console.log("hello user");
+
+function changeColor(a) {
+    let color;
+  if (a >= 0 && a < 0.1) {
+    color = "red";
+    return color;
+  } else if (a >= 0.1 && a < 0.2) {
+    color = "orange";
+    return color;
+  } else if (a >= 0.2 && a < 0.3) {
+    color = "yellow";
+    return color;
+  } else if (a >= 0.3 && a < 0.4) {
+    color = "green";
+    return color;
+  } else if (a >= 0.4 && a < 0.5) {
+    color = "cyan";
+    return color;
+  } else if (a >= 0.5 && a < 0.6) {
+    color = "blue";
+    return color;
+  } else if (a >= 0.6 && a < 0.7) {
+    color = "indigo";
+    return color;
+  } else if (a >= 0.7 && a < 0.8) {
+    color = "violet";
+    return color;
+  } else if (a >= 0.8 && a < 0.9) {
+    color = "silver";
+    return color;
+  } else if (a >= 0.9 && a <= 1.0) {
+    color = "gold";
+    return color;
+  }
+}
+
+let a  = Math.random();
+console.log(a);
+let rang1 = changeColor(a);
+console.log(rang1);
+document.getElementsByClassName("box1")[0].style.backgroundColor = rang1;
+
+
+let b  = Math.random();
+console.log(b);
+let rang2 = changeColor(b);
+console.log(rang2);
+document.getElementsByClassName("box2")[0].style.backgroundColor = rang2;
+
+let c  = Math.random();
+console.log(c);
+let rang3 = changeColor(c);
+console.log(rang3);
+document.getElementsByClassName("box3")[0].style.backgroundColor = rang3;
+
+let d  = Math.random();
+console.log(d);
+let rang4 = changeColor(d);
+console.log(rang4);
+document.getElementsByClassName("box4")[0].style.backgroundColor = rang4;
+
+let e  = Math.random();
+console.log(e);
+let rang5 = changeColor(e);
+console.log(rang5);
+document.getElementsByClassName("box5")[0].style.backgroundColor = rang5;
+
